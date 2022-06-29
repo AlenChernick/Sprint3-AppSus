24a6d639b94685b312972f349f449a8cf6ac5bca
Alon Kolker <alonkolk@gmail.com>
2022-06-29 20:48:29 +0300

    extended mail fixed-not final

--- js/apps/mail/cmps/mail-side-bar.cmp.js
+++ js/apps/mail/cmps/mail-side-bar.cmp.js
@@ -1,13 +1,13 @@
 export default {
  props: ["emails"],
  template: `
- <section>
-
+ <section v-if='emails'>
+<div class="mail-side-bar">
 <div class="inbox-btn">inbox {{CountunReaden}}</div>
 <div class="starred-btn">Starred</div>
 <div class="sent-btn">Sent</div>
 <div class="Draft">Draft</div>
-
+</div>
  </section>
 `,
 data() {
@@ -19,8 +19,7 @@ created() {},
 methods: {},
 computed: {
     CountunReaden(){
-// console.log(this.emails);    
-          return this.unReaded = this.emails.filter((email) => {email.isRead === false}).length
+          return this.unReaded = this.emails.filter((email) => email.isRead === false).length
     }
 },
 unmounted() {},
