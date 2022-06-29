f42fb746b6da3531fa2312abd6f77c7bd18af3fe
AlenChernick <crazyguy771@gmail.com>
2022-06-29 10:46:52 +0300

    vue added

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const { createApp } = Vue
+
+const options = {
+    template: `
+`,
+    data() {
+        return {};
+    },
+    created() { },
+    methods: {},
+    computed: {},
+    unmounted() { },
+};
+
+const app = createApp(options)
+app.mount('#app')
